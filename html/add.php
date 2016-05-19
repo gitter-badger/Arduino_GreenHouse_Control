3b782109cbbed3547377d80233d7194fcd3b8dc0
Nick_Arm <armenisnick@hotmail.com>
2016-05-19 19:21:14 +0300

    Add files via upload

---
+++ html/add.php
@@ -0,0 +1,26 @@
+<?php
+   	include("connect.php");
+   	
+   	$link=Connection();
+
+	$temp1=$_POST["temp1"];
+	$hum1=$_POST["hum1"];
+	$lum1=$_POST["lum1"];
+
+	$query = "INSERT INTO `tempLog` (`temperature`, `humidity`, `luminosity`) 
+		VALUES ('".$temp1."','".$hum1."','".$lum1."')"; 
+   	
+   	mysql_query($query,$link);
+	
+	$countrows = mysql_query("SELECT * FROM tempLog");
+	if (mysql_num_rows($countrows) > 20){
+		
+		$deletequery = "DELETE FROM tempLog WHERE ID ORDER BY timeStamp ASC LIMIT 1";
+		mysql_query($deletequery,$link);
+	}
+
+	mysql_close($link);
+
+	
+   	header("Location: index.php");
+?>
